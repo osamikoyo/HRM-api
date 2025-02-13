a16f0f4776af38c9ba1aa24dafadcc9858df72ff
osamikoyo <v.pylayev@mail.ru>
2025-02-13 07:39:35 +0500

    add register routes

--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	golang.org/x/net v0.33.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
+	golang.org/x/time v0.8.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20241202173237-19429a94021a // indirect
 	google.golang.org/grpc v1.70.0 // indirect
 	google.golang.org/protobuf v1.36.5 // indirect
--- internal/vocation/routes.go
+++ internal/vocation/routes.go
@@ -1,1 +1,49 @@
 package vocation
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/osamikoyo/hrm-api/internal/config"
+	"github.com/osamikoyo/hrm-api/internal/vocation/routes"
+)
+
+func RegisterRoutes(e *echo.Echo, cfg  *config.Config) {
+	cc, err := InitClient(cfg)
+	if err != nil{
+		e.Logger.Panic(err)
+	}
+
+	vocs := e.Group("/vocation")
+	vocs.Use(middleware.Logger())
+
+	vocs.POST("/add", cc.Add)
+	vocs.POST("/get", cc.Get)
+	vocs.POST("/delete", cc.Delete)
+}
+
+func (c *Client) Add(ctx echo.Context) error {
+	err := routes.Add(ctx, c.client)
+	if err != nil{
+		return ctx.String(http.StatusInternalServerError, err.Error())
+	}
+	return ctx.String(http.StatusCreated, "success")
+}
+
+func (c *Client) Get(ctx echo.Context) error {
+	resp, err := routes.Get(ctx, c.client)
+	if err != nil{
+		return ctx.String(http.StatusInternalServerError, err.Error())
+	}
+
+	return ctx.JSON(http.StatusOK, resp)
+}
+
+func (c *Client) Delete(ctx echo.Context) error {
+	err := routes.Delete(ctx, c.client)
+	if err != nil{
+		return ctx.String(http.StatusInternalServerError, err.Error())
+	}
+	return ctx.String(http.StatusOK, "success")
+}
